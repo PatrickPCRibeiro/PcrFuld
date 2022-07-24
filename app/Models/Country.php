e08913ce27bde2191e77caeb3c7877ba27c1fa45
PatrickPCRibeiro <patrick.coimbra.ribeiro@gmail.com>
2022-07-24 17:44:46 -0300

    alteração no model Country

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Country extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'description',
+        'abreviation'
+    ];
+
+protected $table = 'countries';
+protected $primaryKey = 'id';
+protected $keyType = 'int';
+protected $incrementing = true;
 }
--- database/migrations/2022_07_09_003400_create_countries_table.php
+++ database/migrations/2022_07_09_003400_create_countries_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
             $table->string('description',40);
-            $table->string('abbreviation',2);
+            $table->string('abreviation',2);
             $table->timestamps();
         });
     }
