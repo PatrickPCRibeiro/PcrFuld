921a4a0758810cc4c399237ac5b05b1d472b957c
PatrickPCRibeiro <patrick.coimbra.ribeiro@gmail.com>
2022-07-24 17:40:50 -0300

    alteração model city

--- app/Models/City.php
+++ app/Models/City.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     use HasFactory;
+protected $fillable = [
+'id',
+'name',
+'uf_id'
+
+];
+
+protected $table = 'cities';
+protected $primaryKey = 'id';
+protected $keyType = 'int';
+public $incrementing = true;
+
+
 }
