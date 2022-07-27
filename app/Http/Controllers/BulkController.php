c825d81995e05467f6e6895bfd5429b881832d97
PatrickPCRibeiro <patrick.coimbra.ribeiro@gmail.com>
2022-07-27 05:56:01 -0300

    configurando routers

--- app/Http/Controllers/BulkController.php
+++ app/Http/Controllers/BulkController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BulkRequest;
+use App\Models\Bulk;
 use Illuminate\Http\Request;
 
-class BulkController extends Controller
-{
-    //
+class BulkController extends Controller{
+
+    public function index()
+    {
+        $date = Bulk::all();
+        return response()->json($date);
+    }
+
+    public function show($slug)
+    {
+        $date = Bulk::find($slug);
+        return response()->json($date);
+    }
+
+    public function store()
+    {
+        echo "store";
+    }
+
+    public function update()
+    {
+        echo "update";
+    }
+
+    public function delete()
+    {
+        echo "delete";
+    }
+
 }
